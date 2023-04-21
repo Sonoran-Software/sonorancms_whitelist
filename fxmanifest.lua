5012463bf88d4b3788335aa21c9e8d4ab56bdd57
Max Michaels <max32268@gmail.com>
2023-04-20 19:11:16 -0500

    update formatting

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Sonoran Software'
+real_name 'Sonoran CMS Whitelist'
+description 'Syncs Whitelisting permissions with Sonoran CMS'
 version '1.2.3'
-git_repo 'https://github.com/Sonoran-Software/sonoran_whitelist'
+git_repo 'https://github.com/Sonoran-Software/sonorancms_whitelist'
 
 server_script 'sv_whitelist.js'
 
